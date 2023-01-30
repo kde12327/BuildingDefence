eda901d19a4d90654431b672923b6cc68ad79ee0
kde12327 <kde12327@gmail.com>
2023-01-30 17:35:37 +0900

    Create project Add some test levels

---
+++ Source/BuildingDefence/public/BuildingDefenceGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BuildingDefenceGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BUILDINGDEFENCE_API ABuildingDefenceGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
